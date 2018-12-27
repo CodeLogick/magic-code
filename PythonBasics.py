8b0bdf853eb03e8506a72c371a185adff8f2d101
CodeLogick <43672951+CodeLogick@users.noreply.github.com>
2018-12-27 19:49:46 +0530

    Update PythonBasics.py

--- PythonBasics.py
+++ PythonBasics.py
@@ -14,4 +14,16 @@ for w in st.split(' '):
     wordDict[w] = wordDict[w] + 1
 for k,v in wordDict.items():
   print(f'{k}, {v} times')
+  
+  
+# number guess magic
+input('Think any int number..')
+input('Add the same num from your friend..')
+from random import randint
+myNumber = randint(20,100)
+input('Add my {} to the total..'.format(myNumber))
+input('Now give half to god..')
+input("Return your friend's share to him/her..")
+print('The answer is {}'.format(myNumber / 2))
+
 
